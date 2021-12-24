798bffb58e86af4c2e57a0b51e5e691c866220ea
H.Alawneh <halawneh595@gmail.com>
2021-12-24 20:46:56 +0200

    add expo modules to Android and iOS

--- README.md
+++ README.md
@@ -1,23 +1,50 @@
 # Chatsy
+
 ### Installation
+
 ```sh
 $ git clone https://github.com/hasanal1995/chat-app-reactnative.git
 $ cd chat-app-reactnative
 $ yarn
 $ cd ios && pod install
 ```
+
 ### Running for Android
+
 ```sh
 $ yarn android
 ```
+
 ### Running for iOS
+
 ```sh
 $ yarn ios
 ```
+
 ### Plugins
 
 Chatsy is currently extended with the following plugins.
 
-| Plugin | Read more |
-| ------ | ------ |
-| React Native Navigation/Wix | [https://github.com/wix/react-native-navigation]
+| Plugin                      | Read more                                        |
+| --------------------------- | ------------------------------------------------ |
+| React Native Navigation/Wix | [https://github.com/wix/react-native-navigation] |
+
+# Expo OTA (over the air updates)
+
+### supported release channels:
+
+- `staging-channel`: used for test (etc: QA testing).
+- `prod-v1`: used for production.
+
+### steps to publish:
+
+- go to `Expo.plist` and change `EXUpdatesReleaseChannel` to `release-channel-name`.
+- go to `AndroidManifest.xml` and change `expo.modules.updates.EXPO_RELEASE_CHANNEL` to `release-channel-name`.
+- run `expo publish --release-channel release-channel-name`.
+  ,
+
+### get last published release channel details:
+
+- `https://exp.host/@zenhr/zenhr/index.exp?release-channel={release-channel-name}&sdkVersion=44.0.0`
+
+---
--- src/components/notification-listener/notification-listener.tsx
+++ src/components/notification-listener/notification-listener.tsx
@@ -8,7 +8,6 @@
 import { useEffect } from 'react';
 import messaging from '@react-native-firebase/messaging';
 import { StorageHelper } from 'helperes';
-import { Platform } from 'react-native';
 
 /**
  * A function component wih no UI to listen to notifications.
@@ -29,32 +28,39 @@ function NotificationListener() {
    * Requests permission for iOS.
    */
   const requestUserPermission = async () => {
-    const authStatus = await messaging().requestPermission({
-      sound: true,
-      announcement: true,
-      criticalAlert: true,
-      alert: true,
-      badge: false,
-      provisional: true,
-    });
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-    if (enabled) {
-      const tokenFromStorage = await StorageHelper.get('@fcmToken');
-      if (!tokenFromStorage) {
-        messaging()
-          .getToken()
-          .then(async token => {
-            StorageHelper.save('@fcmToken', token).catch(e => {
-              console.log('ERROE: save token to storage', e);
+    console.log('try to req permission');
+    try {
+      const authStatus = await messaging().requestPermission({
+        sound: true,
+        announcement: true,
+        criticalAlert: true,
+        alert: true,
+        badge: false,
+        provisional: true,
+      });
+      console.log('authStatus', authStatus);
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+      console.log('enabled', enabled);
+      if (enabled) {
+        const tokenFromStorage = await StorageHelper.get('@fcmToken');
+        if (!tokenFromStorage) {
+          messaging()
+            .getToken()
+            .then(async token => {
+              console.log('device token is', token);
+              StorageHelper.save('@fcmToken', token).catch(e => {
+                console.log('ERROE: save token to storage', e);
+              });
+            })
+            .catch(e => {
+              console.log('ERROR: generate fcm token', e);
             });
-          })
-          .catch(e => {
-            console.log('ERROR: generate fcm token', e);
-          });
+        }
       }
+    } catch (e) {
+      console.log('ERROR: e');
     }
   };
 
